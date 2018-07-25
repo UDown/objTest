da3ac53edcdb76d520fe015ed043217c38328c9f
tstone <ex-yanlei002@pingan.com.cn>
2018-07-25 23:27:22 +0800

    初始化工程

---
+++ gittest/gittest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by tstone on 2018/7/25.
+//  Copyright © 2018年 tstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gittest/gittest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  gittest
+//
+//  Created by tstone on 2018/7/25.
+//  Copyright © 2018年 tstone. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
